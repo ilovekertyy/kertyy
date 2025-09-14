d87d1a40355b76dc5a57dbe2d79dd63181f7d7ab
ilovekertyy <krebikertyy@gmail.com>
2025-09-14 16:04:56 +0700

    first commit

---
+++ js/message.js
@@ -0,0 +1,25 @@
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = ('I LOVE YOU SO MUCH KERTY!').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+    setTimeout(() => {
+      window.location.href = "papers.html";
+    }, 9000);
+  }, 1000);
+};
